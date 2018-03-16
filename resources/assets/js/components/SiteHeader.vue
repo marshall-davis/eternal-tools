38a481146b4a6b31cb5251e1cd7fa0d9dad96bf2
Marshall Davis <toothlessrebel@gmail.com>
2018-03-16 00:18:19 -0400

    Adds layout components and view.
     Provides basic navigation and layout.

---
+++ resources/assets/js/components/SiteHeader.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="main-header">
+        <h1 class="ui main header">Eternal City Tools</h1>
+        <span class="header">A Collection of Useful Tools for The Eternal City</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "site-header"
+    }
+</script>
+
+<style scoped>
+
+</style>
