8ba8734ce789fb2b29480ab66b106ac7b4ad211b
Marshall Davis <toothlessrebel@gmail.com>
2018-03-17 01:41:33 -0400

    Updates Vue to produce results.

--- resources/assets/js/components/Characters/BackstoryGenerator.vue
+++ resources/assets/js/components/Characters/BackstoryGenerator.vue
@@ -1,12 +1,61 @@
 <template>
     <div>
-        This generates a back-story for you.
+        <div v-if="!character">
+            Loading...
+        </div>
+        <div v-else>
+            <div>{{ character }}</div>
+            <div @click="buildCharacter" class="clickable">I don't like that one, gimme another.</div>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "backstory-generator"
+        name: "backstory-generator",
+        data: () => {
+            return {
+                skills: [],
+                nationalities: [],
+                traits: [],
+                adjectives: [],
+                character: ''
+            }
+        },
+        methods: {
+            buildCharacter: function () {
+                let first_adjective = this.pickRandom(this.adjectives) + ',';
+                this.character = [
+                    'You should play',
+                    this.isVowel(first_adjective.substr(0, 1)) ? 'an' : 'a',
+                    first_adjective,
+                    this.pickRandom(this.adjectives),
+                    this.pickRandom(this.nationalities),
+                    this.pickRandom(this.skills),
+                    'who',
+                    this.pickRandom(this.traits),
+                    'and',
+                    this.pickRandom(this.traits)
+                ].join(' ') + '.';
+            },
+            pickRandom: function (items) {
+                return items[Math.floor(Math.random() * items.length)].text;
+            },
+            isVowel: function (letter) {
+                letter = letter.toLowerCase();
+                return letter === "a" || letter === "e" || letter === "i" || letter === "o" || letter === "u";
+            }
+        },
+        mounted() {
+            axios.get('/api/backstories').then((response) => {
+                this.skills = response.data.skills;
+                this.nationalities = response.data.nationalities;
+                this.adjectives = response.data.adjectives;
+                this.traits = response.data.traits;
+
+                this.buildCharacter();
+            })
+        }
     }
 </script>
 
--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -4,7 +4,7 @@ $(() => {
     $('.launch').on('click', function () {
         $('.sidebar').sidebar('setting', 'transition', 'overlay').sidebar('toggle');
     });
-    console.log(vm);
+
     $('#main-navigation').on('click', '.item:not(.header)', (event) => {
         vm.page = $(event.currentTarget).data('page')
     });
