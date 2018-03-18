22b3a8ca07ed8e8254623782477e289a04e45d81
Marshall Davis <toothlessrebel@gmail.com>
2018-03-17 22:23:12 -0400

    Add copy to welcome view.
    This provides actual content and text to the welcome view
    including the basic features that exist or are currently planned.

--- resources/assets/js/components/Welcome.vue
+++ resources/assets/js/components/Welcome.vue
@@ -1,7 +1,34 @@
 <template>
     <div>
-        This is a bunch of content that just say crap.<br>
-        <h1>WHO THE FUCK IS MY TEC CHARACTER?</h1>
+        <div>
+            Provided here is a collection of tools useful as companions to the Skotos Tech Inc. game
+            <em>The Eternal City</em>. Each of these provide some way to improve the play experience but do
+            <strong>not</strong> provide an advantage in game.
+        </div>
+        <div class="ui bulleted list">
+            <div class="item">
+                <div class="ui header">
+                    <i class="map signs icon"></i>Maps
+                </div>
+                <div class="ui list">
+                    <div class="item">Easy to read and download map images that scale well.</div>
+                    <div class="item">
+                        Map <strong>creation</strong> tools that don't require tedious copy and pasting in a photo
+                        editing
+                        software.
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="ui header">
+                    <i class="user outline icon"></i>Character Tools
+                </div>
+                <div class="list">
+                    <div class="item">A character idea generator full of interesting randomized traits and descriptions.
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
