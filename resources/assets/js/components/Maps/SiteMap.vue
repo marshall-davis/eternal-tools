e9e524026ca3a5047a1899c8de5b40dada31fbf7
Marshall Davis <toothlessrebel@gmail.com>
2018-03-20 21:09:53 -0400

    f

--- resources/assets/js/components/Maps/SiteMap.vue
+++ resources/assets/js/components/Maps/SiteMap.vue
@@ -110,6 +110,7 @@
                 let new_context = new_canvas.getContext('2d');
                 this.redraw(new_context);
                 let link = document.createElement('a');
+                console.log(new_canvas, new_context);
                 link.addEventListener('click', () => {
                     link.href = new_canvas.toDataURL();
                     link.download = "map.png";
