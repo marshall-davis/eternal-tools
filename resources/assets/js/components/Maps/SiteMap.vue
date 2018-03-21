c3c6eb782269fd5368452ccafcde5fe7efad9155
Marshall Davis <toothlessrebel@gmail.com>
2018-03-21 02:49:41 -0400

    Allows for proper scaling to preserve grid layout.
     Force clicks to be adjacent to previous room. Allows for scaling to
    properly space between rooms.

--- resources/assets/js/components/Maps/SiteMap.vue
+++ resources/assets/js/components/Maps/SiteMap.vue
@@ -32,7 +32,8 @@
                 totalTranslation: {
                     x: 0,
                     y: 0
-                }
+                },
+                centerPosition: {},
             }
         },
         computed: {
@@ -70,11 +71,17 @@
                 this.findMapPosition();
 
                 Vue.nextTick(() => {
-                    this.changePosition(
-                        Math.floor((this.$refs.canvas.width / 2)),
-                        Math.floor((this.$refs.canvas.height / 2))
-                    );
+                    this.centerPosition = {
+                        x: Math.floor((this.$refs.canvas.width / 2)),
+                        y: Math.floor((this.$refs.canvas.height / 2))
+                    };
+                    this.changePosition(this.centerPosition);
 
+                    this.addDelta({
+                        position: this.position,
+                        size: this.size,
+                        color: this.canvasContext.fillStyle
+                    });
                     this.draw();
                 });
 
@@ -95,6 +102,7 @@
                 };
             },
             draw: function () {
+                console.log('Drawing at', this.position.x, this.position.y);
                 let boxPosition = {
                     x: Math.floor(this.position.x - (this.roomSize / 2)),
                     y: Math.floor(this.position.y - (this.roomSize / 2)),
@@ -128,8 +136,8 @@
             startDragging: function (event) {
                 this.dragging = true;
                 this.dragStart = {
-                    x: event.pageX - this.mapPosition.x,
-                    y: event.pageY - this.mapPosition.y
+                    x: event.pageX,
+                    y: event.pageY
                 };
             },
             stopDragging: function () {
@@ -142,8 +150,8 @@
                 if (this.dragging) {
                     this.findMapPosition();
                     let position = {
-                        x: event.pageX - this.mapPosition.x,
-                        y: event.pageY - this.mapPosition.y
+                        x: event.pageX,
+                        y: event.pageY
                     };
 
                     let translation = {
@@ -151,46 +159,148 @@
                         y: position.y - this.dragStart.y
                     };
 
+                    console.log('Translating', translation);
                     this.canvasContext.translate(translation.x, translation.y);
                     this.totalTranslation = {
                         x: this.totalTranslation.x + translation.x,
                         y: this.totalTranslation.y + translation.y
                     };
+                    console.log('Total Translation', this.totalTranslation);
                     this.dragStart = position;
                     this.redraw();
                 }
             },
+            possibleRooms: function () {
+                let divisor = this.deltas[this.deltas.length - 1] ? this.deltas[this.deltas.length - 1].size : this.size;
+                let perSide = this.size / divisor;
+                let possiblePoints = {};
+
+                // 1  2  3  4  5
+                // 16          6
+                // 15          7
+                // 14          8
+                // 13 12 11 10 9
+                possiblePoints.ne = this.shiftDirection('ne');
+                possiblePoints.se = this.shiftDirection('se');
+                possiblePoints.nw = this.shiftDirection('nw');
+                possiblePoints.sw = this.shiftDirection('sw');
+
+                if (perSide === 1 || perSide === 3) {
+                    possiblePoints.n = this.shiftDirection('n');
+                    possiblePoints.e = this.shiftDirection('e');
+                    possiblePoints.s = this.shiftDirection('s');
+                    possiblePoints.w = this.shiftDirection('w');
+                }
+
+                if (perSide === 2 || perSide === 3) {
+                    let nudge = this.size / 4;
+                    possiblePoints.nNudgeE = {
+                        x: possiblePoints.n.x + nudge,
+                        y: possiblePoints.n.y
+                    };
+                    possiblePoints.nNudgeW = {
+                        x: possiblePoints.n.x - nudge,
+                        y: possiblePoints.n.y
+                    };
+                    possiblePoints.eNudgeN = {
+                        x: possiblePoints.e.x,
+                        y: possiblePoints.e.y + nudge
+                    };
+                    possiblePoints.eNudgeS = {
+                        x: possiblePoints.e.x,
+                        y: possiblePoints.e.y - nudge
+                    };
+                    possiblePoints.sNudgeE = {
+                        x: possiblePoints.s.x + nudge,
+                        y: possiblePoints.s.y
+                    };
+                    possiblePoints.sNudgeW = {
+                        x: possiblePoints.s.x - nudge,
+                        y: possiblePoints.s.y
+                    };
+                    possiblePoints.wNudgeN = {
+                        x: possiblePoints.w.x,
+                        y: possiblePoints.w.y + nudge
+                    };
+                    possiblePoints.wNudgeS = {
+                        x: possiblePoints.w.x,
+                        y: possiblePoints.w.y - nudge
+                    };
+                }
+
+                return possiblePoints;
+            },
+            findClosetPossibleTo(x, y) {
+                let closest = undefined;
+                let currentMinimumDistance = undefined;
+                let possibilities = this.possibleRooms();
+
+                for (const direction in possibilities) {
+                    if (possibilities.hasOwnProperty(direction)) {
+                        let distance = Math.sqrt(Math.pow((x - possibilities[direction].x), 2) + Math.pow(y - possibilities[direction].y, 2));
+
+                        if (distance < currentMinimumDistance || currentMinimumDistance === undefined) {
+                            currentMinimumDistance = distance;
+                            closest = direction;
+                        }
+                    }
+                }
+
+                return closest;
+            },
             drawClick: function (event) {
-                this.changePosition(
+                let direction = this.findClosetPossibleTo(
                     event.pageX - this.mapPosition.x - this.totalTranslation.x,
                     event.pageY - this.mapPosition.y - this.totalTranslation.y
                 );
+
+                this.changePosition(this.shiftDirection(direction));
                 this.addDelta({
                     position: this.position,
+                    direction: direction,
                     size: this.size,
                     color: this.canvasContext.fillStyle,
-                    path: undefined
                 });
                 this.draw();
+                this.markPosition();
             },
-            changePosition: function (x, y) {
+            changePosition: function (to) {
                 this.position = {
-                    x: x,
-                    y: y
+                    x: to.x,
+                    y: to.y
                 };
+                this.markPosition();
             },
-            drawLine: function (fromPoint, toPoint) {
+            markPosition: function () {
+                console.log(this.position.x, this.position.y)
+            },
+            drawLine: function (fromPoint, toPoint, color) {
                 console.log('Drawing from', fromPoint.x, fromPoint.y, 'to', toPoint.x, toPoint.y);
+                let originalStroke = this.canvasContext.strokeStyle;
+                let originalLineWidth = this.canvasContext.lineWidth;
+                this.canvasContext.strokeStyle = color;
+                this.canvasContext.lineWidth = 5;
                 this.canvasContext.beginPath();
                 this.canvasContext.moveTo(fromPoint.x, fromPoint.y);
                 this.canvasContext.lineTo(toPoint.x, toPoint.y);
                 this.canvasContext.stroke();
+                this.canvasContext.strokeStyle = originalStroke;
+                this.canvasContext.lineWidth = originalLineWidth;
             },
             undo: function () {
                 console.log('Removing last');
                 let last = this.deltas.pop();
                 if (last) {
-                    this.canvasContext.clearRect(last.position.x, last.position.y, last.size, last.size);
+                    let size = last.size * this.scale;
+                    this.canvasContext.clearRect(
+                        last.position.x - (size / 2),
+                        last.position.y - (size / 2),
+                        size,
+                        size
+                    );
+
+                    let previous = this.deltas[this.deltas.length - 1];
+                    this.changePosition(previous ? previous.position : this.centerPosition);
                 }
             },
             walk: function (path) {
@@ -200,30 +310,39 @@
                         let direction = segment.substr(0, segment.search(/\d/));
                         let distance = segment.substr(segment.search(/\d/));
                         for (let pace = 0; pace < distance; pace++) {
-                            this.shiftDirection(direction);
+                            this.changePosition(this.shiftDirection(direction));
                             this.addDelta({
                                 position: this.position,
+                                direction: direction,
                                 size: this.size,
                                 color: this.canvasContext.fillStyle,
-                                path: undefined
                             });
                             this.draw();
                         }
                     });
+                this.draw();
+                this.markPosition();
             },
             shiftDirection: function (direction) {
+                let newPosition = {
+                    x: this.position.x,
+                    y: this.position.y
+                };
+
                 if (['n', 'ne', 'nw'].includes(direction)) {
-                    this.changePosition(this.position.x, this.position.y - (this.size * this.scale) - 2);
+                    newPosition.y = newPosition.y - (this.size * this.scale) - 2;
                 }
                 if (['e', 'ne', 'se'].includes(direction)) {
-                    this.changePosition(this.position.x + (this.size * this.scale) + 2, this.position.y);
+                    newPosition.x = newPosition.x + (this.size * this.scale) + 2;
                 }
                 if (['s', 'se', 'sw'].includes(direction)) {
-                    this.changePosition(this.position.x, this.position.y + (this.size * this.scale) + 2);
+                    newPosition.y = newPosition.y + (this.size * this.scale) + 2;
                 }
                 if (['w', 'sw', 'nw'].includes(direction)) {
-                    this.changePosition(this.position.x - (this.size * this.scale) - 2, this.position.y);
+                    newPosition.x = newPosition.x - (this.size * this.scale) - 2;
                 }
+
+                return newPosition;
             },
             setMode: function (mode) {
                 this.mode = mode;
@@ -237,13 +356,14 @@
                 this.canvasContext.restore();
             },
             redraw: function () {
+                this.position = this.centerPosition;
                 this.clearCanvas();
 
                 let originalFillStyle = this.canvasContext.fillStyle;
                 let originalSize = this.size;
                 this.deltas.forEach((delta) => {
                     this.canvasContext.fillStyle = delta.color;
-                    this.changePosition(delta.position.x, delta.position.y);
+                    this.changePosition(this.shiftDirection(delta.direction));
                     this.size = delta.size;
                     if (delta.path) {
                         this.walk(delta.path);
@@ -280,7 +400,8 @@
             update: function () {
                 axios.put('/api/maps/' + this.map, {
                     steps: JSON.stringify(this.deltas),
-                }).then((response) => {});
+                }).then((response) => {
+                });
             },
             scaleTo: function (scale) {
                 this.scale = scale;
