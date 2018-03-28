f332852a3f5feccaaf48cca55f94c6f99e5221ea
Marshall Davis <toothlessrebel@gmail.com>
2018-03-28 02:07:18 -0400

    Adds relationship table for label_ticket.

---
+++ database/migrations/2018_03_28_054332_add_labels_tickets_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddLabelsTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('label_ticket', function (Blueprint $table) {
+            $table->integer('ticket_id');
+            $table->integer('label_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('label_ticket');
+    }
+}
