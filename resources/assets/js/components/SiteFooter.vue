0473843982100e7cdda41ef28e7dbb954e6ba7d4
Marshall Davis <toothlessrebel@gmail.com>
2018-06-15 23:05:56 -0400

    Adds Paypal donation.
     Adds form and method for allowing for Paypal donations.

--- resources/assets/js/components/SiteFooter.vue
+++ resources/assets/js/components/SiteFooter.vue
@@ -1,13 +1,38 @@
 <template>
     <div class="main-footer">
-        <div><a href="http://www.skotos.net/games/eternal-city/" title="The Eternal City Game">The Eternal City</a> is copyright Worlds Apart Productions LLC & Skotos Tech. Inc.</div>
-        <div>Want to help out? Donate for server costs.</div>
+        <div>
+            <a href="http://www.skotos.net/games/eternal-city/" title="The Eternal City Game">The Eternal City</a> is
+            copyright Worlds Apart Productions LLC & Skotos Tech. Inc.
+        </div>
+        <div>
+            <em>Eternal City Tools</em> is not affiliated with Worlds Apart Productions LLC, Skotos Tech Inc. or The Eternal City.
+        </div>
+        <div>
+            Want to help <em>Eternal City Tools</em>? <a href="" v-on:click.stop.prevent="donate">Donate</a> for server costs or submit a
+            pull request on <a href="https://github.com/ToothlessRebel/eternal-tools"><i class="github icon"></i></a>.
+        </div>
+
+        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" ref="donationForm">
+            <input type="hidden" name="cmd" value="_donations">
+            <input type="hidden" name="business" value="4CGK7824JJXN8">
+            <input type="hidden" name="lc" value="US">
+            <input type="hidden" name="item_name" value="Exposure Software">
+            <input type="hidden" name="item_number" value="Eternal Tools">
+            <input type="hidden" name="currency_code" value="USD">
+            <input type="hidden" name="bn" value="PP-DonationsBF:btn_donate_SM.gif:NonHosted">
+        </form>
     </div>
 </template>
 
 <script>
     export default {
-        name: "site-footer"
+        name: "site-footer",
+        methods: {
+            donate: function () {
+                console.log('Donating!');
+                $(this.$refs.donationForm).submit();
+            },
+        },
     }
 </script>
 
@@ -26,5 +51,9 @@
 
     .main-footer a {
         pointer-events: auto;
+    }
+
+    .main-footer .github.icon {
+        margin: 0;
     }
 </style>
