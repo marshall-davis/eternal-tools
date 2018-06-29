ec02c20613d9451b059a9bbe431065a235f342c4
Marshall Davis <toothlessrebel@gmail.com>
2018-06-28 23:11:58 -0400

    Adds toastr to the application.
     This installs the NPM module for toastr and includes the
    necessary files in the distributed files.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,7 +8,7 @@
 
     <title>Eternal City Tools</title>
 
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body class="pushable">
 @component('menu')
@@ -22,5 +22,7 @@
     </div>
 </div>
 </body>
-<script src="{{ asset('/js/app.js') }}"></script>
+<script src="{{ mix('/js/manifest.js') }}"></script>
+<script src="{{ mix('/js/vendor.js') }}"></script>
+<script src="{{ mix('/js/app.js') }}"></script>
 </html>
